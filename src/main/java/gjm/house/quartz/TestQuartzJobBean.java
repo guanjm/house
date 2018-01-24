37e94b7655787bd4f25e8f1c7f57f171a6b148fc
Tim <Tim@172.22.40.75>
2018-01-24 17:28:50 +0800

    update

---
+++ src/main/java/gjm/house/quartz/TestQuartzJobBean.java
@@ -0,0 +1,14 @@
+package gjm.house.quartz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestQuartzJobBean {
+	
+	private static Logger log = LoggerFactory.getLogger(TestQuartzJobBean.class);
+	
+	public void execute(){
+		log.error("------------------------");
+	}
+
+}
